99ef4db8517ce7f74613d1578854639fd9d57a4b
alukoayodele <ayodelealuko009.aa@gmail.com>
2021-05-07 20:44:55 +0100

    env jawsdb

---
+++ config/config.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: "root",
+    password: "AYOkunnumi@009",
+    database: "fundallMiniProject",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    use_env_varable: "JAWSDB_URL",
+    dialect: "mysql",
+  },
+};
