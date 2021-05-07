a64c61ed03b71cfba46c6110bd017a391f99ad62
alukoayodele <ayodelealuko009.aa@gmail.com>
2021-05-07 20:26:11 +0100

    add prestart

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const app = express();
 app.options("*", cors());
 app.use(cors());
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.json({ message: `You hit mini-project home route` });
 });
--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 import app from "./app";
-let server;
 let port = 3001;
 let { NODE_ENV, PORT: productionPort, IP: productionIP } = process.env;
 
 if (NODE_ENV === "development") {
-  server = app.listen(port, () => {
+  app.listen(port, () => {
     console.log(`Live server running in on port ${port}`);
   });
 } else {
-  server = app.listen(productionPort, productionIP, () => {
+  app.listen(productionPort, productionIP, () => {
     console.log(`Live server running heroku ${productionIP}:${productionPort}`);
   });
 }
-
-export default server;
